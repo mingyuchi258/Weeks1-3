acf03a4fd691e4afebad74ddb68fd2b96b3c8b3a
Mingyu Chi <chimi@shernet.sheridancollege.ca>
2026-01-12 11:42:43 -0500

    curve

--- Assets/Scenes/New Folder/1.cs
+++ Assets/Scenes/New Folder/1.cs
@@ -1,24 +1,32 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     float speed = 0.1f;
+    Vector2 left;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        left = Camera.main.ScreenToWorldPoint(Vector2.zero);
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 newPosition = transform.position;
-        newPosition.x += speed;
+        newPosition.x += speed * Time.deltaTime;
         transform.position = newPosition;
 
         //check if the position.x < 0 or position.x > width of the screen
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
-        if (screenPos.x < 0 || screenPos.x > Screen.width)
+        if (screenPos.x < 0)
+        {
+            speed = -speed;
+        }
+
+        if (screenPos.x > Screen.width)
         {
             speed = -speed;
         }
