9d0cd7c45240e0263ac93ae634b7dddbdedc61cc
Mingyu Chi <chimi@shernet.sheridancollege.ca>
2026-01-12 11:50:48 -0500

    move

--- Assets/Scenes/New Folder/1.cs
+++ Assets/Scenes/New Folder/1.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    float speed = 0.1f;
+    public float speed = 0.1f;
     Vector2 left;
+    Vector2 right;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         left = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        right = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width,Screen.height));
     }
 
     // Update is called once per frame
@@ -17,18 +19,21 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         Vector2 newPosition = transform.position;
         newPosition.x += speed * Time.deltaTime;
-        transform.position = newPosition;
+        
 
         //check if the position.x < 0 or position.x > width of the screen
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
         if (screenPos.x < 0)
         {
+            newPosition.x = left.x;
             speed = -speed;
         }
 
         if (screenPos.x > Screen.width)
         {
+            newPosition.x = right.x;
             speed = -speed;
         }
+        transform.position = newPosition;
     }
 }
