516f9f0ab52ca93277550bf896dd5b702220e6ea
Mingyu Chi <chimi@shernet.sheridancollege.ca>
2026-01-23 11:18:42 -0500

    Making aliens move

---
+++ Assets/Scenes/sf/move.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newP = transform.position;
+        newP.x += speed * Time.deltaTime;
+        transform.position = newP;
+
+        Vector2 screenP = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenP.x < 70)
+        {
+            speed = speed * -1;
+        }
+
+        if (screenP.x >Screen.width - 70)
+        {
+            speed = speed * -1;
+        }
+    }
+}
