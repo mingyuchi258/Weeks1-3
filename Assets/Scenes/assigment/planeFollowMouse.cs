7e2fbe6b0610851938955b42290e9042a753ca27
Mingyu Chi <chimi@shernet.sheridancollege.ca>
2026-01-24 22:33:25 -0500

    make the plane follow the mouse x-axis movement

---
+++ Assets/Scenes/assigment/planeFollowMouse.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class planeFollowMouse : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Make the plane follow the mouse movement
+        Vector2 mouseP = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        //Fix the aircraft so that it cannot move up and down
+        mouseP.y = -3;
+        transform.position = mouseP;
+    }
+}
